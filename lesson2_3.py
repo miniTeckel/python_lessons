795065e4dc58a4efe7642f4afc9a133bb8e5a6b5
Lana Ka <lana@home>
2017-09-20 21:49:58 +0300

    formatting fixed

--- lesson2_3.py
+++ lesson2_3.py
@@ -3,23 +3,24 @@ import os
 from pprint import pprint
 import chardet
 
+
 def read_news_file_json(file_name):
     with open(file_name, "rb") as cb_file:
         data = cb_file.read()
         encoding = chardet.detect(data)
-        s = data.decode (encoding['encoding'])
+        s = data.decode(encoding['encoding'])
         return json.loads(s)
 
 
 def string_stats(string, stat):
     for word in string.split(" "):
         if len(word) >= 6:
-            word=word.lower()
+            word = word.lower()
             if word in stat:
-                stat[word]+=1
+                stat[word] += 1
             else:
-                stat[word]=1
-            
+                stat[word] = 1
+
 
 def count_words(news):
     words_stat = {}
@@ -30,12 +31,15 @@ def count_words(news):
         string_stats(title, words_stat)
     return words_stat
 
+
 def top_10(stat):
     s = sorted(stat.items(), key=lambda x: x[1], reverse=True)
     return s[:10]
 
+
 def get_json_files(dir_path):
     return [os.path.join(dir_path, f) for f in os.listdir(dir_path) if os.path.splitext(f)[1] == ".json"]
+
 
 if __name__ == "__main__":
     for path in get_json_files("."):
