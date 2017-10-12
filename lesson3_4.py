96ba58f2c78d876a67511c53fe063e0a4318b908
Lana Ka <lana@home>
2017-10-13 00:34:36 +0300

    updated

--- lesson3_4.py
+++ lesson3_4.py
@@ -1,7 +1,10 @@
 import requests
+import time
 
 VK_URL = "https://api.vk.com/method/"
 VK_API_VER = "5.52"
+MAX_REQ_PER_SEC = 3
+MAX_REQ = 100
 
 
 def get_access_token(path="./vk_access_token.txt"):
@@ -18,9 +21,30 @@ def get_all_friends(token, user_id=None):
         params["user_id"] = user_id 
 
 
-    response = requests.get(URL, params=params)
+    response = requests.get("%s%s" %(VK_URL, "friends.get"), params=params)
     json_ = response.json()
-    return ''.join(json_['text'])
+    if "response" in json_:
+        return json_['response']['items']
+    print(json_)
+    return []
+
+
+def check_limits(req_count):
+    if req_count % MAX_REQ_PER_SEC == 0:
+        time.sleep(1)
+    return req_count < MAX_REQ
+
 
 if __name__ == "__main__":
-    get_all_friends(get_access_token())
+    access_token = get_access_token()
+    my_friends = get_all_friends(access_token)
+    common_friends = set(my_friends)
+    req_count = 1
+    for id in my_friends:
+        if not check_limits(req_count):
+            break
+        his_friends = get_all_friends(access_token, id)
+        req_count += 1
+        if his_friends:
+            common_friends = set(his_friends) & common_friends
+    print(common_friends)
